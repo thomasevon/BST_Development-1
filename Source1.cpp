58692144e60aae20e890e6bc05b137f303d266f4
thomasevon <56008706+thomasevon@users.noreply.github.com>
2019-10-01 09:27:01 -0600

    Add files via upload
    First Version, Schuster Version

---
+++ Source1.cpp
@@ -0,0 +1,107 @@
+//Binary Search Tree
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Node {
+	int data;
+	Node* left, * right;
+};
+
+class BST {
+private:
+	Node* root;
+	Node* insert(int, Node*); //helper
+	int getSize(Node*); //helper
+	void displayInOrder(Node*); //helper
+	//  void deleteNode(int, Node*); // helper
+public:
+	BST();
+	void insert(int);
+	void displayInOrder();
+	int getSize();
+	// void deleteNode(int); 
+}; //BST class
+
+//helper functions
+Node* BST::insert(int v, Node* r) { // "r" is short for "root"
+	if (r == nullptr) {
+		r = new Node;
+		r->left = r->right = nullptr;
+		r->data = v;
+		return r;
+	}
+	else if (v < r->data) {
+		r->left = insert(v, r->left);
+		return r;
+	}
+	else {
+		r->right = insert(v, r->right); // r->right is assigned the new node that will be created to the right
+		return r;
+	}
+} //insert helper
+
+void BST::displayInOrder(Node* r) {
+	if (r != nullptr)
+	{
+		displayInOrder(r->left);
+		cout << r->data << endl;
+		displayInOrder(r->right);
+	}
+} //displayInOrder helper
+
+int BST::getSize(Node* r) {
+	if (r == nullptr)
+		return 0;
+	else
+		return 1 + getSize(r->left) + getSize(r->right);
+} //getSize helper
+
+//constructors
+BST::BST() {
+	root = nullptr;
+} //BST
+
+
+//setters
+void BST::insert(int v) {
+	root = insert(v, root);
+} //insert
+
+//getters
+int BST::getSize() {
+	return getSize(root);
+} //getSize
+
+//utility
+void BST::displayInOrder() {
+	displayInOrder(root);
+} //displayInOrder
+
+
+int main() {
+	BST bst1 = BST();
+	cout << "Size: " << bst1.getSize() << endl << endl;
+
+	bst1.insert(20);
+	bst1.displayInOrder();
+	cout << "Size: " << bst1.getSize() << endl << endl;
+
+	bst1.insert(10);
+	bst1.insert(30);
+	bst1.displayInOrder();
+	cout << "Size: " << bst1.getSize() << endl << endl;
+
+	bst1.insert(5);
+	bst1.insert(40);
+	bst1.insert(25);
+	bst1.displayInOrder();
+	cout << "Size: " << bst1.getSize() << endl << endl;
+
+	bst1.insert(0);
+	bst1.insert(2);
+	bst1.insert(-5);
+	bst1.insert(-2);
+	bst1.displayInOrder();
+	cout << "Size: " << bst1.getSize() << endl << endl;
+} //main
