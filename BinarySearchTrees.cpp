ea063a317573077bcf03e71edd24d6726461e04f
thomasevon <56008706+thomasevon@users.noreply.github.com>
2019-10-03 08:44:30 -0600

    Add files via upload
    Sorry I forgot to commit this, this has all my functions in it. 
    
    int main() is not complete though and looks like trash haha

--- BinarySearchTrees.cpp
+++ BinarySearchTrees.cpp
@@ -12,10 +12,12 @@ struct Node {
 class BST {
 private:
 	Node* root;
-	Node* insert(int, Node*); //helper
-	int getSize(Node*); //helper
-	void displayInOrder(Node*); //helper
+	Node* insert(int, Node*); // helper
+	int getSize(Node*); // helper
+	void displayInOrder(Node*); // helper
+	void displayPreOrder(Node*); // helper
 	//  void deleteNode(int, Node*); // helper
+	void display(Node*, int); // display helper
 public:
 	BST();
 	void insert(int);
@@ -23,12 +25,15 @@ public:
 	int getSize();
 	int maxValue();
 	Node* find(int);
-	// void deleteNode(int); 
+	void deleteNode(int); 
 	void load(int, int, int);
+	int returnRoot();
+	void displayPreOrder(); 
+	void display(); 
 }; //BST class
 
 //helper functions
-Node* BST::insert(int v, Node* r) { // "r" is short for "root"
+Node* BST::insert(int v, Node* r) { 
 	if (r == nullptr) {
 		r = new Node;
 		r->left = r->right = nullptr;
@@ -46,8 +51,7 @@ Node* BST::insert(int v, Node* r) { // "r" is short for "root"
 } //insert helper
 
 void BST::displayInOrder(Node* r) {
-	if (r != nullptr)
-	{
+	if (r != nullptr) {
 		displayInOrder(r->left);
 		cout << r->data << endl;
 		displayInOrder(r->right);
@@ -55,10 +59,8 @@ void BST::displayInOrder(Node* r) {
 } //displayInOrder helper
 
 int BST::getSize(Node* r) {
-	if (r == nullptr)
-		return 0;
-	else
-		return 1 + getSize(r->left) + getSize(r->right);
+	if (r == nullptr) return 0;
+	else return 1 + getSize(r->left) + getSize(r->right);
 } //getSize helper
 
 //constructors
@@ -72,6 +74,10 @@ void BST::insert(int v) {
 } //insert
 
 void BST::load(int howMany, int minVal, int maxVal) {
+	if (root == nullptr) {
+		int newRoot = (minVal + maxVal) / 2;
+		insert(newRoot);
+	}
 	insert(minVal);
 	insert(maxVal);
 	howMany--;
@@ -88,9 +94,6 @@ void BST::load(int howMany, int minVal, int maxVal) {
 int BST::getSize() {
 	return getSize(root);
 } //getSize
-
-//loaders
-
 
 //utility
 void BST::displayInOrder() {
@@ -120,6 +123,86 @@ Node* BST::find(int v) {
 	}
 	cout << v << " is not in this BST!" << endl; 
 	return nullptr; 
+}
+
+void BST::deleteNode(int v) {
+	Node* target = root;
+	Node* parent = nullptr;
+
+	// find node
+	while (target != nullptr) {
+		if (v > target->data) {
+			parent = target;
+			target = target->right;
+		}
+		else if (v < target->data) {
+			parent = target;
+			target = target->left;
+		}
+		else {
+			if (target->left == nullptr && target->right == nullptr) { //delete leaf Node
+				if (parent->left == target) parent->left = nullptr;
+				else parent->right = nullptr;
+				return;
+			}
+			else if (target->left == nullptr || target->right == nullptr) { //delete node w/ one child
+				if (target->left == nullptr) parent->left = target->right;
+				else if (target->right == nullptr) parent->right = target->left;
+			}
+			else { // delete node that has two children
+				Node* runner = target->right;
+				while (runner->left != nullptr) runner = runner->left;
+				target->data = runner->data;
+			}
+			return;
+		}
+	}
+}
+
+
+void BST::displayPreOrder(Node* r) {
+	if (root == nullptr)
+	{
+		cout << "ERROR: Tree does not exist" << endl;
+		return;
+	}
+	if (r != nullptr)
+	{
+		cout << r->data << "  " << endl;
+		displayPreOrder(r->left);
+		displayPreOrder(r->right);
+	}
+}
+
+void BST::displayPreOrder() {
+	displayPreOrder(root);
+}
+
+void BST::display(Node* ptr, int level)
+{
+	if (ptr != nullptr)
+	{
+		display(ptr->right, level + 1);
+		cout << endl;
+		if (ptr == root)
+			cout << "Root->:  ";
+		else
+		{
+			for (int i = 0; i < level; i++)
+				cout << "       ";
+		}
+		cout << ptr->data;
+		display(ptr->left, level + 1);
+	}
+} // display helper
+
+void BST::display() {
+	display(root, 1);
+}
+
+
+int BST::returnRoot() {
+	return root->data; 
 }
 
 int main() {
@@ -159,11 +242,27 @@ int main() {
 	Node* testPtr = bst1.find(findTarget);
 	cout << "found: " << testPtr->data; 
 
-	cout << "test void load(int, int, int) " << endl << endl;
-	BST bst2 = BST();
-	bst2.load(5, 2, 9);
-	bst2.displayInOrder();
+	bst1.display();
+	cout << endl << "size is: " << bst1.getSize();
 
+	cout << endl << "test void load(4, 2, 8) " << endl << endl;
+	bst1.load(4, 2, 8);
+
+	bst1.display(); 
+
+	cout << endl << "new size is: " << bst1.getSize();
+
+	cout << "test load(int,int,int) with an empty BST " << endl;
+	BST bst2 = BST();
+	bst2.load(4, 1, 10);
+	bst2.display();
+
+	cout << endl << endl << "test deleteNode " << endl;
+	cout << "display before" << endl;
+	bst1.display();
+	bst1.deleteNode(20); 
+	cout << endl << endl << "display after " << endl;
+	bst1.display();
 
 } //main
 
